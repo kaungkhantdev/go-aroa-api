b93dd619f2faf32f2247adc661766004f9695fb6
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-10-09 11:03:30 +0700

    add: user service

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -1,6 +1,9 @@
 package repository
 
-import "go-aora-api/internal/models"
+import (
+	"go-aora-api/internal/database"
+	"go-aora-api/internal/models"
+)
 
 type UserRepository struct {}
 
@@ -18,5 +21,21 @@ func (repo *UserRepository) Create(data CreateData) models.User {
 		Avatar: data.Avatar,
 	}
 
+	database.DB.Create(&user)
 	return user;
+}
+
+func (repo *UserRepository) FindById(id int) models.User {
+	user := models.User{ID: uint(id)}
+
+	database.DB.First(&user)
+	return user;
+}
+
+
+func (repo *UserRepository) FindAll() []models.User {
+	users := []models.User{}
+
+	database.DB.Find(&users)
+	return users;
 }
