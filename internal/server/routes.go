0193956df3a3c1c7fc4d5dd217126eb97956add4
kaungkhantcodigo <kaungkhantzaw@codigo.co>
2024-10-12 22:41:16 +0700

    add: jwt package and testing

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"go-aora-api/pkg/jwt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +14,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 
 	r.GET("/", s.HelloWorldHandler)
+	r.POST("/verify", s.VerifyToken)
 
 	// apiGroup := r.Group("/api")
 	// apiGroup.Use(middleware.AuthMiddleware()) 
@@ -34,5 +37,37 @@ func (s *Server) HelloWorldHandler(c *gin.Context) {
 	resp["token"] = token;
 	resp["message"] = "Hello World"
 
+	c.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) VerifyToken(c *gin.Context) {
+	authHeader := c.GetHeader("Authorization")
+	if authHeader == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+		c.Abort()
+		return
+	}
+
+	tokenString := strings.TrimPrefix(authHeader, "Bearer ");
+	if tokenString == authHeader {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Bearer token required"})
+		c.Abort()
+		return
+	}
+
+	claims, err := jwt.VerifyJWT(tokenString)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		c.Abort()
+		return
+	}
+
+	resp := map[string]string{}
+	resp["auth_header"] = authHeader;
+	resp["token"] = tokenString;
+	
+	userId := fmt.Sprintf("User Id: %d", claims.UserId)
+	resp["user_id"] = userId
+	
 	c.JSON(http.StatusOK, resp)
 }
